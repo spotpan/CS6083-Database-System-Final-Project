baed5793a433a7061c1645c9a6a23dd344c6a00b
zunchenhuang <zh946@nyu.edu>
2017-05-06 23:42:50 -0400

    login and register

---
+++ front-end/js/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by admin on 5/5/2017.
+ */
+
+$("#loginform").click(function(){
+    $("#login").css('display','block');
+    $("#register").css('display','none');
+});
+
+$("#registerform").click(function (){
+    $("#login").css('display','none');
+    $("#register").css('display','block');
+});
+
+
+
+$(document).mouseup(function (e)
+{
+    var container = $(".login");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.toggle();
+        $('#loginform').removeClass('green');
+    }
+});
