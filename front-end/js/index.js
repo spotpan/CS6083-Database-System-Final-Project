b3f3dc654aed2074ab1b8c042042635bdc8e96f2
zunchenhuang <zh946@nyu.edu>
2017-05-06 23:51:34 -0400

    index.js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -14,14 +14,14 @@ $("#registerform").click(function (){
 
 
 
-$(document).mouseup(function (e)
-{
-    var container = $(".login");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.toggle();
-        $('#loginform').removeClass('green');
-    }
-});
+// $(document).mouseup(function (e)
+// {
+//     var container = $(".login");
+//
+//     if (!container.is(e.target) // if the target of the click isn't the container...
+//         && container.has(e.target).length === 0) // ... nor a descendant of the container
+//     {
+//         container.toggle();
+//         $('#loginform').removeClass('green');
+//     }
+// });
